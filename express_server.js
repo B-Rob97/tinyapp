2433b078b9fbc30624dffc6f472874b4c0d09ec3
Brett Roberts <brett.roberts@live.ca>
2023-09-05 15:40:45 -0600

    Add client-side form for updating long URLs and edit links to URLs index; Implement server-side POST route for URL updates

--- express_server.js
+++ express_server.js
@@ -23,44 +23,62 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// Home
 app.get('/', (req, res) => { // * Will need to change this *
   res.send('Hello!');
 });
 
+// New
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.post("/urls", (req, res) => {
+// Post New URL
+app.post("/urls/", (req, res) => {
   const shortURL = generateRandomString();
   const longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
 
   console.log(req.body);
   console.log(urlDatabase);
-  res.redirect(`/urls/${shortURL}`);
+  res.redirect(`/urls/`);
 });
 
+// All URLs
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+// Specific URL
+app.get('/urls/:id', (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+// Garbage?
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+// Edit URL => Redirect to all URLs
+app.post(`/urls/:id`, (req, res) => {
+  const shortURL = req.params.id;
+  const longURL = req.body.longURL;
+
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls`);
+});
+
+
+// Delete a URL => Redirect to All URLs
 app.post(`/urls/:shortURL/delete`, (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
   res.redirect('/urls');
 });
 
+// Redirects to actual long URL of the short URL ID
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
@@ -69,6 +87,8 @@ app.get("/u/:id", (req, res) => {
 
 
 
+
+// Listen
 app.listen(PORT, (req, res) => {
   console.log(`App is listening on ${PORT}!`);
 });
