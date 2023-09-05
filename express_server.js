a6c66b279b2eed1b0f6db374da31bf9edd4b46d2
Brett Roberts <brett.roberts@live.ca>
2023-09-04 19:06:06 -0600

    Added urls_shows.ejs file in views. Added route into express_server.js. Added link to long URL in urls_shows.ejs HTML code. Added a button to create a new short URL

--- express_server.js
+++ express_server.js
@@ -13,13 +13,21 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get('/urls/:id', (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+
 
 app.listen(PORT, (req, res) => {
   console.log(`App is listening on ${PORT}!`);
